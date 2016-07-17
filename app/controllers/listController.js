699930bcbe15722f0a0cec33b0f61b5ea1919172
Deepak <dkafle@gmail.com>
2016-07-17 19:03:45 +0000

    download functionality added

--- app/controllers/listController.js
+++ app/controllers/listController.js
@@ -26,6 +26,21 @@ function ($scope, $http, $timeout, $location, appService) {
             $location.path('/sign-in');
         }
     };
+    fileList.download = function (index) {
+        var filename = fileList.localItems[index];
+        var endpoint = 'https://express-service-dkafle.c9users.io/download';
+        var query = '?filename=' + filename + '&username=' + appService.username;
+        var url = endpoint + query;
+        if (appService.isAuthenticated) {
+            $http.get(url, function(data) {
+                console.log('downloading...');
+                var blob = new window.Blob([data], {type: "text/csv"});
+                window.saveAs(blob, filename);
+            });
+        } else {
+            $location.path('/sign-in');
+        } 
+    };
     //get file as soon as view loads
     fileList.getLocalFiles();
     fileList.getFiles();
